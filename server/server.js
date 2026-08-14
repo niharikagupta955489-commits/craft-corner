856df74a83141093c4655f747b65ec3117d5cd5c
Niharika <niharikagupta955489@gmail.com>
2026-08-14 22:39:44 +0530

    ne 2

--- server/server.js
+++ server/server.js
@@ -1,20 +1,17 @@
 import dotenv from "dotenv";
 
 dotenv.config({
-  path:"./.env"
+  path: "./.env"
 });
-console.log(process.env);
 
 import cloudinary from "./config/cloudinary.js";
-
 import app from "./app.js";
 import connectDB from "./config/db.js";
-
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server Running on Port ${PORT}`);
 });
